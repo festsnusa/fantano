02afc3e7c03d246612e9f017a0682d6e0e29d294
festsnusa <lukatimof@mail.ru>
2023-05-26 20:40:45 +0700

    added Reviews

--- src/views/ReviewsView.vue
+++ src/views/ReviewsView.vue
@@ -1,19 +1,42 @@
 <template lang="pug">
 AppHeader(:currentIndex="1")
+.reviews
+  section.reviews__left
+    .reviews__header 
+      span.reviews__title Reviews
+    RouterLink.review(v-for="(item, index) in reviews.slice().reverse()" :to="`/review/${item.id}`")
+      img(:src="item.imgSrc", :alt="item.title")
+      span.review__title {{ item.title }}
+  aside.reviews__right
+    AppFilter
 AppFooter
 </template>
 
 <script>
-import AppHeader from '@/components/AppHeader.vue';
-import AppFooter from '@/components/AppFooter.vue';
+import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
+import AppFilter from '@/components/AppFilter.vue'
+
+import json from '@/assets/data/reviews.json'
 
 export default {
   name: "ReviewsView",
   components: {
     AppHeader,
     AppFooter,
-  }
+    AppFilter,
+  },
+  data() {
+    return {
+      reviews: json,
+    }
+  },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.reviews {
+  padding: 3rem;
+  display: flex;
+}
+</style>

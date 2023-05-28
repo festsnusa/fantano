1058df4befd794408a7025f28187c8e477f432c5
festsnusa <lukatimof@mail.ru>
2023-05-28 10:21:29 +0700

    moved data jsons

--- src/views/ListWeekView.vue
+++ src/views/ListWeekView.vue
@@ -2,15 +2,12 @@
 AppHeader(:currentIndex="4")
 .list-week 
   h1.title YEAR END LISTS
-  .list-week__albums 
-    h1.title ALBUMS
-      .thumbnail(v-for="item in albums")
-        img.thumbnail__img(:src="item.imgSrc" :alt="item.year")
-        span.thumbnail__caption {{ item.year }}
+  .list-week__albums
+    AppThumbnail(title="albums" :data="albums")
   .list-week__singles 
-    h1.title SINGLES
+    AppThumbnail(title="singles" :data="singles")
   .list-week__eps 
-    h1.title EPs
+    AppThumbnail(title="eps" :data="EPs")
   .list-week__honorable-mentions
     h1.title HONORABLE MENTIONS
   .list-week__worst-albums 
@@ -23,31 +20,24 @@ AppFooter
 <script>
 import AppHeader from '@/components/AppHeader.vue';
 import AppFooter from '@/components/AppFooter.vue';
+import AppThumbnail from '@/components/AppThumbnail.vue'
+
+import albumsJson from '@/assets/data/loved-list_albums.json'
+import singlesJson from '@/assets/data/loved-list_singles.json'
+import EpsJson from '@/assets/data/loved-list_EPs.json'
 
 export default {
   name: "ListWeekView",
   components: {
     AppHeader,
     AppFooter,
+    AppThumbnail,
   },
   data() {
     return {
-      albums: [
-        { "year": "2022", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/2052.jpg" },
-        { "year": "2021", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/1861.jpg" },
-        { "year": "2020", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/1665.jpg" },
-        { "year": "2019", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/1409.jpg" },
-        { "year": "2018", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/1183.jpg" },
-        { "year": "2017", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/950.jpg" },
-        { "year": "2016", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/730.jpg" },
-        { "year": "2015", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/782.jpg" },
-        { "year": "2014", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/781.jpg" },
-        { "year": "2013", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/783.jpg" },
-        { "year": "2012", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/169.jpg" },
-        { "year": "2011", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/784.jpg" },
-        { "year": "2010", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/785.jpg" },
-        { "year": "2010s", "imgSrc": "https://cdn.albumoftheyear.org/list/thumbs/1455.jpg" },
-      ]
+      albums: albumsJson,
+      singles: singlesJson,
+      EPs: EpsJson,
     }
   }
 }

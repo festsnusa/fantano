494c3933e55e512d2867dcb9228e2947845cdcf3
festsnusa <lukatimof@mail.ru>
2023-05-28 10:21:08 +0700

    new component

---
+++ src/components/AppThumbnail.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+h1.title {{ title }}
+  RouterLink.thumbnail(v-for="item in data" :to="`/list-week/${title}-${item.year}`")
+    img.thumbnail__img(:src="item.imgSrc" :alt="item.year")
+    span.thumbnail__caption {{ item.year }}
+</template>
+
+<script>
+export default {
+  name: "AppThumbnail",
+  props: ["title", "data"],
+}
+</script>
+
+<style lang="scss" scoped></style>

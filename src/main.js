ad53c645bc48f70dc26e5657b21b7ea93c5fed12
festsnusa <lukatimof@mail.ru>
2023-05-31 21:26:03 +0700

    removed packages

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 // library.add(faMoon)
 
@@ -15,6 +15,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.component('font-awesome-icon', FontAwesomeIcon)
+// app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')

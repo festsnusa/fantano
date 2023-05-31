f4ac14f51fc80593df5c19b22f55a4ddca51fb05
festsnusa <lukatimof@mail.ru>
2023-05-31 18:02:59 +0700

    font-awesome registration

--- src/main.js
+++ src/main.js
@@ -6,12 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// import LazyTube from "vue-lazytube"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// library.add(faMoon)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-// app.use(LazyTube)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')

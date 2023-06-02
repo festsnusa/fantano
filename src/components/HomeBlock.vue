67709f9b993b8345d1be9296d8bdf970769b2941
festsnusa <lukatimof@mail.ru>
2023-06-02 22:45:02 +0700

    changed styles

--- src/components/HomeBlock.vue
+++ src/components/HomeBlock.vue
@@ -4,7 +4,7 @@
   RouterLink(:to="`/${link}`")
     span.header__show Show all
 .content
-  RouterLink.content__element(v-for="(item, index) in arr" v-show="index<4" :to="`/review/${item.id}`")
+  RouterLink.content__element(v-for="(item, index) in arr" v-show="index<5" :to="`/review/${item.id}`")
     AppPreloader(v-show="!item.imageShow")
     img.content__image(v-show="item.imageShow" :src="getThumbnail(item.video)", :alt="item.title" @load="item.imageShow = true")
     p.content__title {{ item.title }}
@@ -32,4 +32,37 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.content {
+  display: flex;
+  align-items: baseline;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  gap: 2rem;
+
+  &__element,
+  &__image {
+    max-width: 19rem;
+  }
+
+  &__image {
+    border-radius: 10px;
+    box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
+
+    &:hover {
+      transform: scale(1.05);
+      transition: all 0.5s ease-in;
+    }
+  }
+}
+
+@media (max-width: 752px) {
+  .content {
+
+    &__element,
+    &__image {
+      max-width: 100%;
+    }
+  }
+}
+</style>

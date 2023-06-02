004a35bc6940b08a050d54614b873a7a5b280833
festsnusa <lukatimof@mail.ru>
2023-06-02 15:24:28 +0700

    added pinia-plugin-persistedstate

--- src/main.js
+++ src/main.js
@@ -2,19 +2,17 @@ import './assets/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-// library.add(faMoon)
-
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
+
+app.use(pinia)
 app.use(router)
-// app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')

31e6cca5654ff92385a690e654637a78b21e57c8
festsnusa <lukatimof@mail.ru>
2023-06-02 17:15:16 +0700

    persist year

--- src/components/AppFilter.vue
+++ src/components/AppFilter.vue
@@ -1,23 +1,36 @@
 <template lang="pug">
 .search
-  //- font-awesome-icon.search__icon(icon="fa-solid fa-magnifying-glass")
   <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
   input.search__input(type="text" v-model="text" @input="searchReview(text)" placeholder="Search for a review...")
-  select.search__select(name="select" @change="filter")
+  select.search__select(v-model="year" name="select" @change="filter")
     option(value="") Filter by year
     option(v-for="item in years" :value="item") {{item}}
 </template>
 
 <script>
+import { mapStores } from 'pinia'
+import useYearStore from '@/stores/year'
+
 export default {
   name: "AppFilter",
   props: ["filter", "searchReview"],
   data() {
     return {
       text: '',
-      years: [2023, 2022, 2021, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010],
+      year: '',
+      years: [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010],
     }
   },
+  computed: {
+    ...mapStores(useYearStore)
+  },
+  mounted() {
+    this.year = this.yearStore.currentYear
+
+    this.yearStore.$subscribe((mutation, state) => {
+      this.year = state.currentYear
+    })
+  }
 }
 </script>
 

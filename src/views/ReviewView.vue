04f98de7a3ad45b1c04057d9b392f4ca176ede3e
festsnusa <lukatimof@mail.ru>
2023-06-02 23:49:28 +0700

    styling for mobile devices

--- src/views/ReviewView.vue
+++ src/views/ReviewView.vue
@@ -2,8 +2,8 @@
 AppHeader
 .review
   .review__left
-    YouTube.video(:src="current.video" @ready="onReady" ref="youtube")
-    <iframe style="border-radius:12px" :src="`${current.spotify}&theme=0`" width="100%" height="452" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    YouTube.video(:src="current.video" @ready="onReady" ref="youtube" :width="calculateWidth")
+    iframe.spotify(v-if="current.spotify !=''" style="border-radius:12px" :src="`${current.spotify}&theme=0`" width="100%" height="452" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy")
   .review__right
     .review__header
       h1.review__title {{ current.title }} ({{ current.year }})
@@ -22,6 +22,7 @@ import ReviewStar from '@/components/ReviewStar.vue'
 
 import YouTube from 'vue3-youtube'
 import Toggle from '@vueform/toggle'
+import { useMediaQuery } from '@vueuse/core'
 
 import json from '../assets/data/main-channel.json'
 
@@ -38,24 +39,68 @@ export default {
       arr: json.filter(e => e.type == 'review'),
       current: null,
       en: true,
+      isLargeScreen: useMediaQuery('(min-width: 1024px)'),
+    }
+  },
+  computed: {
+    calculateWidth() {
+      return this.isLargeScreen ? 640 : 400
+    }
+  },
+  methods: {
+    updateScreen() {
+      this.isLargeScreen = useMediaQuery('(min-width: 1024px)')
     }
   },
   created() {
     this.current = json.filter((e) => e.id == this.$route.params.review)[0]
-  }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .review {
   padding: 3rem;
+  display: grid;
   display: flex;
   justify-content: space-between;
   gap: 3rem;
-  // align-items: center;
+
+  &__left {
+    gap: 3rem;
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+  }
 }
 
 .multiline {
   white-space: pre-wrap;
+}
+
+@media (max-width: 1300px) {
+  .review {
+    flex-direction: column;
+
+    &__left {
+      flex-direction: row;
+    }
+  }
+
+  .video {
+    width: 10rem;
+  }
+
+  .spotify {
+    max-height: 22.5rem;
+  }
+}
+
+@media (max-width: 820px) {
+  .review {
+    &__left {
+      flex-direction: column;
+    }
+  }
 }
 </style>

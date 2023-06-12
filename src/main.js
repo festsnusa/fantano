0df9a977159c78e1c73a3697ecaf4f642fd8a396
festsnusa <lukatimof@mail.ru>
2023-06-12 21:15:12 +0700

    font-awesome

--- src/main.js
+++ src/main.js
@@ -8,10 +8,18 @@ import Markdown from 'vue3-markdown-it'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faSortDown } from '@fortawesome/free-solid-svg-icons'
+library.add(faSortDown)
+
 const app = createApp(App)
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedState)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(pinia)
 app.use(router)

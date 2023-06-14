a152130045d531956868213f1940fd441fe751d6
festsnusa <lukatimof@mail.ru>
2023-06-14 11:10:52 +0700

    ant design registration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './assets/main.scss'
+import 'ant-design-vue/dist/antd.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -10,6 +11,7 @@ import router from './router'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Antd from 'ant-design-vue'
 
 import { faSortDown } from '@fortawesome/free-solid-svg-icons'
 library.add(faSortDown)
@@ -24,5 +26,6 @@ app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(pinia)
 app.use(router)
 app.use(Markdown)
+app.use(Antd)
 
 app.mount('#app')

e33c83f16a2313d627f3423b301c1453b78546c9
festsnusa <lukatimof@mail.ru>
2023-06-15 16:00:24 +0700

    breadcrumb component registration

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,19 @@
+<template>
+  <a-breadcrumb>
+    <a-breadcrumb-item v-for="(item, index) in arr" :key="index">
+      <Router-Link :to="item.link">{{ item.title }}</Router-Link>
+    </a-breadcrumb-item>
+    <a-breadcrumb-item>
+      {{ title }}
+    </a-breadcrumb-item>
+  </a-breadcrumb>
+</template>
+
+<script>
+export default {
+  name: "AppBreadcrumb",
+  props: ["arr", "title"],
+}
+</script>
+
+<style lang="scss" scoped></style>

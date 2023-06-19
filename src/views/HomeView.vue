591bb419fd5f6a316bf8cc04f2dbe76bc6122052
festsnusa <lukatimof@mail.ru>
2023-06-19 16:46:13 +0700

    firebase test functions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <template lang="pug">
+//- <div id="content"></div>
 AppHeader
 Breadcrumb.breadcrumb(title="Home")
 main
@@ -21,6 +22,9 @@ import HomeBlock from '@/components/HomeBlock.vue'
 import AppPreloader from '@/components/AppPreloader.vue'
 
 import json from '@/assets/data/main-channel.json'
+// import { storage } from '../includes/firebase'
+// import { ref, getDownloadURL } from "firebase/storage";
+import { marked } from 'marked'
 
 export default {
   name: "HomeView",
@@ -38,7 +42,71 @@ export default {
       ynr: json.filter(e => e.type == 'YUNOREVIEW').slice(0, 5),
     }
   },
+  methods: {
+    async getFile(downloadURL) {
+      let link = document.createElement('a');
+      link.download = downloadURL;
+
+      let blob = new Blob(downloadURL, { type: 'text/plain' });
+
+      link.href = URL.createObjectURL(blob);
+
+      link.click();
+
+      URL.revokeObjectURL(link.href);
+    },
+    async downloadFile(url) {
+
+      fetch(url, {
+        mode: 'no-cors',
+      })
+        .then((response) => {
+          if (!response.ok) return "There was error with your response, please check the details and try again";
+          return response.text();
+        })
+        .then(md => {
+          // Convert the Markdown to HTML using marked.js
+          console.log(md)
+          let html = marked(md)
+
+          // Display the HTML content
+          document.getElementById('content').innerHTML = html
+        })
+        .catch(error => {
+          console.error('Error:', error)
+        });
+    }
+
+  },
   async created() {
+    // const starsRef = ref(storage, 'texts/0-en.md')
+    // getDownloadURL(starsRef)
+    //   .then((url) => {
+    //     // Insert url into an <img> tag to "download"
+    //     console.log(url)
+    //     this.downloadFile(url)
+    //   })
+    //   .catch((error) => {
+    //     // A full list of error codes is available at
+    //     // https://firebase.google.com/docs/storage/web/handle-errors
+    //     switch (error.code) {
+    //       case 'storage/object-not-found':
+    //         // File doesn't exist
+    //         break;
+    //       case 'storage/unauthorized':
+    //         // User doesn't have permission to access the object
+    //         break;
+    //       case 'storage/canceled':
+    //         // User canceled the upload
+    //         break;
+
+    //       // ...
+
+    //       case 'storage/unknown':
+    //         // Unknown error occurred, inspect the server response
+    //         break;
+    //     }
+    //   });
   }
 }
 </script>

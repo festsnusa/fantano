1ee8146246d4a3f25f1aa387c336edfe7b2d5f94
festsnusa <lukatimof@mail.ru>
2023-06-20 11:14:47 +0700

    adding dist

---
+++ dist/assets/4107-ru-4942520b.js
@@ -0,0 +1,27 @@
+const n=`ЛУЧШИЕ ТРЕКИ НЕДЕЛИ:\r
+Horrendous - Cult of Shaad'oah\r
+Knocked Loose - Deep in the Willow\r
+hemlocke springs - heavun\r
+Code Orange - Grooming My Replacement\r
+Osees - Goon\r
+The Hives - Countdown to Shutdown\r
+Bickle - The Fly\r
+Fleet Foxes - Under Control (The Strokes Cover)\r
+grouptherapy. - Peak\r
+Sugar Pit - Compete\r
+ANOHNI and the Johnsons - Sliver of Ice\r
+\r
+...meh...\r
+TURQUOISEDEATH, Asian Glow - Dive ft. Parannoul\r
+Trippie Redd & Skye Morales - Took My Breath Away\r
+COBRAH - SUCK\r
+Victoria Monét - On My Mama\r
+PUP - How to Live With Yourself\r
+Doja Cat - Attention\r
+The Weeknd, Suzanna Son - Family\r
+\r
+ХУДШИЕ ТРЕКИ НЕДЕЛИ:\r
+Coi Leray - Run It Up\r
+Voices of Fire - JOY (Unspeakable) ft. Pharrell Williams\r
+Imperial Triumphant - Paranoid Android (Radiohead Cover)\r
+`;export{n as default};

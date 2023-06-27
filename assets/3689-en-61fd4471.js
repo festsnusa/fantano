2ac98ab5c66e5e162033d001e5837c26752254de
festsnusa <lukatimof@mail.ru>
2023-06-27 20:43:54 +0700

    adding dist

---
+++ assets/3689-en-61fd4471.js
@@ -0,0 +1,54 @@
+const n=`<details>\r
+  <summary>Show the full list</summary>\r
+  1. Little Simz - Introvert\r
+  2. Spellling - Boys at School\r
+  3. Sharon Van Etten & Angel Olsen - Like I Used To\r
+  4. Low - Days Like These\r
+  5. BROCKHAMPTON - BUZZCUT ft. Danny Brown\r
+  6. James Blake - Say What You Will\r
+  7. Lingua Ignota - Pennsylvania Furnace\r
+  8. JPEGMAFIA - HAZARD DUTY PAY!\r
+  9. Genesis Owusu - Gold Chains\r
+  10. black midi - John L\r
+  11. St. Vincent - Pay Your Way in Pain\r
+  12. Remi Wolf - Quiet on Set\r
+  13. PinkPantheress - Just for Me\r
+  14. Lil Nas X - Montero\r
+  15. Bleachers - How Dare You Want More\r
+  16. Richard Dawson, Circle - Lily\r
+  17. Rina Sawayama - Chosen Family ft. Elton John\r
+  18. Injury Reserve - Knees\r
+  19. Sufjan Stevens, Angelo De Augustine - Cimmerian Shade\r
+  20. Ibeyi, Pa Salieu - Made of Gold\r
+  21. Arca - Madre\r
+  22. Porter Robinson - Musician\r
+  23. Twin Shadow - Lonestar\r
+  24. Taylor Swift - All Too Well (10 Minute Version)\r
+  25. Kirin J. Callinan - Dumb Enough\r
+  26. Rico Nasty - Money ft. Flo Milli\r
+  27. Kero Kero Bonito - The Princess and the Clock\r
+  28. Wet Leg - Chaise Lounge\r
+  29. Zack Fox - fafo\r
+  30. Japanese Breakfast - Be Sweet\r
+  31. bbno$, Rich Brian - Edamame\r
+  32. Magdalena Bay - You Lose!\r
+  33. illuminati hotties - Pool Hopping\r
+  34. Jessie Ware - Hot N Heavy\r
+  35. Alice Longyu Gao, Alice Glass - LEGEND\r
+  36. Silk Sonic - Smokin Out the Window\r
+  37. CupcakKe - Moonwalk\r
+  38. Meridian Brothers, Conjunto Media Luna - La Secta\r
+  39. Tkay Maidza - Kim ft. Baby Tate\r
+  40. Lil Texas - Louder\r
+  41. slowthai - Mazza ft. A$AP Rocky\r
+  42. Yola - Diamond Studded Shoes\r
+  43. Black Country, New Road - Concorde\r
+  44. Amber Mark - Foreign Things\r
+  45. Tokischa, Rosalía - Linda\r
+  46. Pussy Riot, Dorian Electra - TOXIC\r
+  47. Pop Smoke - AP\r
+  48. JID, Denzel Curry - Bruuuh [Remix]\r
+  49. Doja Cat - Kiss Me More ft. SZA\r
+  50. Weezer - All My Favorite Songs\r
+</details>\r
+`;export{n as default};

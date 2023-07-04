4a1c885bf95c1996fa0a7725c7f7d1d0d1a4793d
festsnusa <lukatimof@mail.ru>
2023-07-04 20:26:09 +0700

    adding dist

---
+++ assets/132-en-5bc3df58.js
@@ -0,0 +1,14 @@
+const e=`These guys are a Tennessee band, and my first exposure to them was from downloading the track "Summer" off "Gorilla Versus Bear". And you guys may remember I did a track review of that song on this channel. Enjoyed it quite a bit, but the low baritone-esque vocals reminded me of the band Girls. And considering that them and Girls are on the same record label, and Girls, that debut LP was so huge in the indie community, I thought this might be another Girls' rip-off. Girls and Magic Kids certainly have things in common, with Girls they pull from a similar vintage California sound, they're very summery, very blissful. But I would say Magic Kids are way, way more influenced by the finely tuned orchestrations and production of the great Brian Wilson. I mean, hardly a track goes by where there's not a just very present Beach Boys influence. Maybe one of the strongest tracks that exemplify this is the song "Hey Boy" that sounds a lot like these speedier parts on the song "Wouldn't It Be Nice" with romping organ.\r
+\r
+But even when the band is worshiping the album "Pet Sounds", they still sound like Magic Kids, they still make it their own, whether it's with an interesting interlude. Or to go back to the song "Hey Boy" with a great background chorus, just sounds huge.\r
+\r
+And I read a bio about these guys on True Panther Sounds website and they were likened to Electric Light Orchestra and Belle & Sebastian a little bit too, and I have to agree. But I will warn you, "Memphis" is extremely cheesy, schmaltzy and a little corny too. It's unapologetically upbeat, it does not care if you're in a bad mood, doesn't care if you've had a crappy day because it is completely absorbed in the good time that it is having.\r
+\r
+I know, this sounds cliche, but I want to say that this is one of the most feel-good albums of 2010. And that shows up in the songwriting, in the melodies, but especially in the arrangements: the horns, the strings, the piano, it's just like candy for the ears. The production made me feel like I was listening to something that was just a little bit more than a pop album, something fancy. It's like the difference between fine aged wine and grape soda. And Magic Kids sits somewhere in the middle, maybe a little bit closer to grape soda.\r
+\r
+But I love how straightforward this thing is, it's 29 minutes, light, easygoing.\r
+\r
+FAV TRACKS: SUPERBALL, SUMMER, PHONE, LITTLE RED RADIO, HEY BOY, CANDY\r
+\r
+LIGHT 8\r
+`;export{e as default};

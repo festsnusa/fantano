534d3e24878053ab8dde9baf0d270067d883ac52
festsnusa <lukatimof@mail.ru>
2023-07-05 20:19:08 +0700

    adding dist

---
+++ assets/4117-en-f9e71266.js
@@ -0,0 +1,23 @@
+const r=`ЛУЧШИЕ ТРЕКИ НЕДЕЛИ:\r
+Sampha - Spirit 2.0\r
+Olivia Rodrigo - Vampire\r
+Hozier - Unknown / Nth\r
+Earl Sweatshirt - Making the Band (Danity Kane)\r
+Blur - St. Charles Square\r
+Cannibal Corpse - Blood Blind\r
+Cheekface - (Whats So Funny 'Bout) Peace, Love and Understanding\r
+Chemical Brothers - Live Again ft. Halo Maud\r
+High Pulp - Unified Dakotas ft. Jeff Parker of Tortoise\r
+\r
+...meh...\r
+The Alchemist - RIP Tracy ft. Earl Sweatshirt & billy woods\r
+Charli XCX - Speed Drive\r
+BENEE - DO IT AGAIN (Official FIFA Women’s World Cup ’23 Song) ft. Mallrat\r
+George Clanton - Vapor King / SubReal\r
+James Blake - Big Hammer\r
+Beck, Phoenix - Odyssey\r
+\r
+ХУДШИЕ ТРЕКИ НЕДЕЛИ:\r
+Fall Out Boy - We Didn't Start the Fire\r
+Logic - Check Please ft. C Dot Castro & T Man the Wizard\r
+`;export{r as default};

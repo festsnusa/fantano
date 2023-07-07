531d1d57959522a622883089c4342aabe7fb010c
festsnusa <lukatimof@mail.ru>
2023-07-07 12:06:56 +0700

    adding dist

---
+++ dist/assets/4120-en-68ad8e43.js
@@ -0,0 +1,29 @@
+const e=`I missed the train with their debut back in 2019, but I'm definitely getting on at this stop. that is for sure. This is one of those folksy, cute, somewhat homespun projects that have a super intimate sound and come off as very endearing. Think of the artists that comes across in the music of like Kimya Dawson or Daniel Johnston. But while Joanna's music lands in a similar realm of rawness, there's something more studied and groomed about the performances and songwriting on this LP too, as Joanna clearly has a pretty large base of knowledge to pull from when it comes to old styles of jazz, as well as folk music, reggaeton and spirituals. They generally seem pretty focused on traditional music at large, repurposing those melodies, chords and grooves in their own way to address more modern or, at least, complicated emotional struggles.\r
+\r
+Case in point the track "Stockholm Syndrome", which is a song all about being taken advantage of by someone, as Joanna describes them, they seem sociopathic, emotionally distant as well, as we get descriptions as to how things between these two broke off. The presentation instrumentally on this track is super simple as well between the gently strummed acoustic guitars and Joanna's very peculiar voice. Peculiar, but also bold and stand out, and fitting for the eras of music that Joanna pulls from. I would also say in terms of timbre and tone, fans of Big Thief and Adrianne Lenker may want to take notice too.\r
+\r
+Some of the instrumental palettes on this project are pretty skeletal, but Joanna's writing is often so strong and so narratively fluid, it doesn't need a whole lot of support for it to have an emotional punch. Especially with lyrics like:\r
+\r
+"Did you keep your room dirty so I'd feel like I had the flu\r
+Did you expect me to clean it like your mom must've done for you..."\r
+\r
+The rest of the tracks on this thing are presented in a similarly simple state and showcase more struggles with difficult people and difficult feelings. Be that the title track, which reads like an aspirational pat on the back, or a pep talk about getting over self-loathing and self-deprecation, or an obsession with the sort of worry, as Joanna describes leaves them paralyzed and wasting time. There's also "People Are Toys to You", which is all about an individual who's a person user, going from individual to individual until you get their fill. And Joanna sings about essentially being at the other end of this treatment. All in all, the track is a very direct and cutting character portrait. There's also "Right Here", which is all about being hung up over someone who is gone and may or may not even care about you anymore. Joanna sings lines like:\r
+\r
+"And in one day you'll go and I'll lose you\r
+You won't lose me\r
+...\r
+I won't survive here without you".\r
+\r
+Who hurt you? What happened? That's all I can say. The closer gives me similar vibes as well.\r
+\r
+But the entire record is not doom and gloom, let me assure you. Tracks like "I Will Be With You", as well as "I'll Make You Mine" are genuine and cute songs of love and devotion, with the former sounding like a musical intermission during a local theater production, or school recital in the best way possible. There is a site amateurish feel to it that's cute and makes the emotions behind the tracks feel sweet and innocent. There's a similar vibe coming off of the song "Mountains High", which is a piano ballad held up by very lightning in a bottle, in the moment performance. Joanna laces the track with a message about giving yourself grace, which is a pretty consistent theme throughout the record, and doing your best through whatever emotional struggles you may be facing. "The Love I Give" is a very simple throwback type of folk song, with some thrusty chords and some straightforward words, whole thing sounds like a tumbleweed rolling by during the Dust Bowl era. But the message is about being a loving person, even if you may find yourself in places where you're giving more than you get. Personally, I worry about being taken advantage of with such an approach, but there is an emotional intelligence to this record, that's advocating for pretty consistently throughout the record, that honestly is beautiful and is admirable.\r
+\r
+Instrumentally toward the end, there is an interesting switch up of sorts with the song "The Human Magnet Song", which is a piece of piano rock, it's cool this sort of thing can be pulled off and incorporated into the record, but I'm not entirely sure if Joanna's vocals work as well in this context as the very simple and straightforward, very string kissed ballad. Further criticisms I have the record on the whole is that it is a little brief, sometimes the performances are blemished to a fault, a couple songs feel as if they're written around unhealthy fixations.\r
+\r
+But with all that being said, it's still a very solid collection of tracks, a big quality batch of sweet, endearing songs that have a tradition flair to them.\r
+\r
+FAV TRACKS: I'VE GOT ME, I WILL BE WITH YOU, PEOPLE ARE TOYS TO YOU, DRIFTING ON A CLOUD, STOCKHOLM SYNDROME, RIGHT HERE, THE LOVE I GIVE, THE SONG\r
+LEAST FAV TRACK: THE HUMAN MAGNET SONG\r
+\r
+LIGHT 8\r
+`;export{e as default};

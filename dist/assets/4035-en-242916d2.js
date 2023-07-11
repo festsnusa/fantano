e21683663d91c25e489fc1d7f5da2c5dc205d66e
festsnusa <lukatimof@mail.ru>
2023-07-11 13:34:20 +0700

    adding dist

--- dist/assets/4035-en-242916d2.js
+++
@@ -1,8 +0,0 @@
-const n=`2-pump chump.\r
-\r
-LIL PUMP - LIL PUMP 2 / 2023 / SOUNDCLOUD / GARBAGE\r
-\r
-NOT GOOD/10\r
-\r
-Y'all know this is just my opinion, right?\r
-`;export{n as default};
--- dist/assets/4060-en-aefcc657.js
+++
@@ -1,5 +0,0 @@
-const E=`NF isn't like the other rappers.\r
-\r
-NF - HOPE / 2023 / SELF-RELEASED / POP RAP, CONSCIOUS HIP HOP, I LIKE HOW "EPIC MUSIC" IS A GENRE ON RYM\r
-NOT GOOD/10\r
-`;export{E as default};

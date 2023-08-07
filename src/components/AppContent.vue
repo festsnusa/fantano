b1461e13763b7c602a477ac7a4a0adead8fbdad1
festsnusa <lukatimof@mail.ru>
2023-08-07 17:14:38 +0700

    new method setBorderColor

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -3,7 +3,7 @@
   .content__element(v-if="arr.length" v-for="(item, index) in arr" v-show="index<contentCount")
     RouterLink(:to="`/videos/${item.id}`")
       AppPreloader(v-show="!item.imageShow")
-      img.content__image(v-show="item.imageShow" :src="thumbnail(item.video)", :alt="item.title" @load="item.imageShow = true")
+      img.content__image(v-show="item.imageShow" :class="setBorderColor(item)" :src="thumbnail(item.video)", :alt="item.title" @load="item.imageShow = true")
       p.content__title {{ item.title }}
   .content__element(v-else)
     span No videos
@@ -24,6 +24,15 @@ export default {
       thumbnail: getThumbnail,
     }
   },
+  methods: {
+    setBorderColor(item) {
+      if (item.rating <= 4 || item.rating === "NOT GOOD") return "content__image_red"
+      else if (item.rating <= 6) return "content__image_grey"
+      else if (item.rating === 7) return "content__image_green"
+      else if (item.rating <= 10) return "content__image_yellow"
+      return ""
+    }
+  }
 }
 </script>
 
@@ -47,6 +56,22 @@ export default {
     &:hover {
       transform: scale(1.05);
       transition: all 0.5s ease-in;
+    }
+
+    &_yellow {
+      border: 3px solid #F4F426;
+    }
+
+    &_green {
+      border: 3px solid #00F400;
+    }
+
+    &_grey {
+      border: 3px solid grey;
+    }
+
+    &_red {
+      border: 3px solid #F44B35;
     }
   }
 }
